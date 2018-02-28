8e73f30b58f94e1c2d81dc45a9b572262e03800b
gerardbyrne <gerardbyrne@lineone.net>
2018-02-28 21:56:16 +0000

    Add files via upload

---
+++ GitTest/src/gerry/github/test/TestClass.java
@@ -0,0 +1,11 @@
+package gerry.github.test;
+
+public class TestClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Gerard Byrne");
+		System.out.println("Allstate");
+		System.out.println("Belfast");
+	}
+}
